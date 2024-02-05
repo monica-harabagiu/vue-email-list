5d6d049379753a7fa1c7a47ea345d44ed6110df6
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-02-05 17:11:51 +0100

    bonus print complete email list

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ createApp({
     methods: {
         generateEmail() {
 
+            this.generatedEmail = []
+
             for (let i = 0; i < 10; i++) {
                 
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
