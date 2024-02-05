10982fb6bc8505eaae8c9c90e788db69a1d796cb
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-02-05 17:01:53 +0100

    generate emails

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        message: 'Hello Vue!',
-        image: 'https://picsum.photos/700/600'
-      }
+        return {
+            generatedEmail: [],
+        }
+    },
+    methods: {
+        generateEmail() {
+
+            for (let i = 0; i < 10; i++) {
+                
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                    console.log(result.data)
+                    this.generatedEmail.push(result.data.response) 
+                })
+            }
+        }
     }
-  }).mount('#app')
+}).mount('#app')
